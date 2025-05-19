e9372f48ceb058620bac51f0cee1a62928ced9dc
gotcha <gotcha@LAPTOP-2HTJBFSA>
2025-05-20 01:41:17 +0800

    part2

--- src/record/row.cpp
+++ src/record/row.cpp
@@ -7,21 +7,65 @@ uint32_t Row::SerializeTo(char *buf, Schema *schema) const {
   ASSERT(schema != nullptr, "Invalid schema before serialize.");
   ASSERT(schema->GetColumnCount() == fields_.size(), "Fields size do not match schema's column size.");
   // replace with your code here
-  return 0;
+  uint32_t tot = 0;
+  uint32_t field_count = fields_.size();
+  memcpy(buf + tot, &field_count, sizeof(field_count));
+  tot += sizeof(field_count);
+
+  uint32_t bitmap_size = (field_count + 7) / 8;
+  char *bitmap = buf+tot;
+  memset(bitmap, 0, bitmap_size);
+  tot += bitmap_size;
+  for (uint32_t i = 0; i < field_count; ++i) {
+    if (fields_[i]->IsNull()) {
+      bitmap[i / 8] |= (1 << (i % 8));
+    }
+    else{
+      tot+= fields_[i]->SerializeTo(buf + tot);
+    }
+  }
+  return tot;
 }
 
 uint32_t Row::DeserializeFrom(char *buf, Schema *schema) {
   ASSERT(schema != nullptr, "Invalid schema before serialize.");
   ASSERT(fields_.empty(), "Non empty field in row.");
   // replace with your code here
-  return 0;
+  uint32_t tot = 0;
+  uint32_t field_count;
+  memcpy(&field_count, buf + tot, sizeof(field_count));
+  tot += sizeof(field_count);
+
+  char *bitmap = buf + tot;
+  uint32_t bitmap_size = (field_count + 7) / 8;
+  memset(bitmap, 0, bitmap_size);
+  tot += bitmap_size;
+  fields_.clear();
+  fields_.resize(field_count);
+  for (uint32_t i = 0; i < field_count; ++i) {
+    if (bitmap[i / 8] & (1 << (i % 8))) {
+      tot+=Field::DeserializeFrom(buf + tot, schema->GetColumn(i)->GetType(), &fields_[i],true);
+    }
+    else{
+      tot+=Field::DeserializeFrom(buf + tot, schema->GetColumn(i)->GetType(), &fields_[i],false);
+    }
+  }
+  return tot;
 }
 
 uint32_t Row::GetSerializedSize(Schema *schema) const {
   ASSERT(schema != nullptr, "Invalid schema before serialize.");
   ASSERT(schema->GetColumnCount() == fields_.size(), "Fields size do not match schema's column size.");
   // replace with your code here
-  return 0;
+  uint32_t tot = 0;
+  tot+= sizeof(uint32_t);
+  tot+= (fields_.size() + 7) / 8;
+  for (uint32_t i = 0; i < fields_.size(); ++i) {
+    if(!fields_[i]->IsNull()) {
+      tot += fields_[i]->GetSerializedSize();
+    }
+  }
+  return tot;
 }
 
 void Row::GetKeyFromRow(const Schema *schema, const Schema *key_schema, Row &key_row) {
--- src/record/schema.cpp
+++ src/record/schema.cpp
@@ -5,15 +5,49 @@
  */
 uint32_t Schema::SerializeTo(char *buf) const {
   // replace with your code here
-  return 0;
+  // magic number + length of column + &column
+  uint32_t tot = 0;
+  memcpy(buf + tot, &SCHEMA_MAGIC_NUM, sizeof(uint32_t));
+  tot += sizeof(uint32_t);
+  uint32_t column_count = static_cast<uint32_t>(columns_.size());
+  memcpy(buf + tot, &column_count, sizeof(uint32_t));
+  tot += sizeof(uint32_t);
+  for (const auto &column : columns_) {
+    tot += column->SerializeTo(buf + tot);
+  }
+  return tot;
 }
 
 uint32_t Schema::GetSerializedSize() const {
   // replace with your code here
-  return 0;
+  uint32_t tot = 0;
+  tot += sizeof(uint32_t); // magic number
+  tot += sizeof(uint32_t); // length of column
+  for (const auto &column : columns_) {
+    tot += column->GetSerializedSize();
+  }
+  return tot;
 }
 
 uint32_t Schema::DeserializeFrom(char *buf, Schema *&schema) {
   // replace with your code here
-  return 0;
+  uint32_t tot = 0;
+  uint32_t magic_num;
+  memcpy(&magic_num, buf + tot, sizeof(uint32_t));
+  tot += sizeof(uint32_t);
+  ASSERT(magic_num == SCHEMA_MAGIC_NUM, "Wrong magic number for schema.");
+  
+  uint32_t column_count;
+  memcpy(&column_count, buf + tot, sizeof(uint32_t));
+  tot += sizeof(uint32_t);
+
+  std::vector<Column *> columns;
+  columns.reserve(column_count);
+  for (uint32_t i = 0; i < column_count; ++i) {
+    Column *column;
+    tot += Column::DeserializeFrom(buf + tot, column);
+    columns.push_back(column);
+  }
+  schema = new Schema(columns,true);
+  return tot;
 }
